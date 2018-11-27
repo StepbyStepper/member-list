cacc362a66a5922dc15bf122eaf3a2a3b90e10e6
StepbyStepper <gorbunov.dv.webdev@gmail.com>
2018-11-27 23:33:04 +0300

    Ренедеринг формы

--- src/Form.js
+++ src/Form.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
 
 class From extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.initialState = {
-			name: '',
-			job: ''
-		};
+    this.initialState = {
+      name: '',
+      job: ''
+    };
 
-		this.state = this.initialState;
-	}
+    this.state = this.initialState;
+  }
+
+  handleChange = event => {
+    const {name, value} = event.target;
+
+    this.setState({
+      [name] : value
+    });
+  }
+
+  render() {
+    const { name, job } = this.state;
+
+    return (
+      <form>
+          <label>Name</label>
+          <input 
+              type="text" 
+              name="name" 
+              value={name} 
+              onChange={this.handleChange} />
+          <label>Job</label>
+          <input 
+              type="text" 
+              name="job" 
+              value={job} 
+              onChange={this.handleChange}/>
+      </form>
+    );
+  }
 }
+
+export default Form;
